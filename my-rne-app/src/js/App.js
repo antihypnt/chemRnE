c5e7e14d4990b6e43f9fa3a1b4ae2e97e47256dd
antihypnotic <gwakgyohap@gmail.com>
2024-05-27 10:52:31 +0900

    readme 수정

--- my-rne-app/src/js/App.js
+++ my-rne-app/src/js/App.js
@@ -4,17 +4,24 @@ export default function App() {
 
     // CSS 안돼서 포기함
     const buttonStyle = {
+        width: "100px",
+        height: "40px",
         background: "#275efe",
         color: "#fff",
         fontSize: "15px",
-        fontFamily: "roboto",
+        fontFamily: "Roboto",
         display: "inline-block",
-        borderRadius: "20px",
-        border: "none"
+        borderRadius: "30px",
+        border: "none",
+        margin: "10px 25px 0px 25px"
+    }
+
+    const divStyle = {
+        textAlign: "center"
     }
 
     return(
-        <div>
+        <div style={divStyle}>
             <button style={buttonStyle}>MODE 1</button>
             <button style={buttonStyle}>MODE 2</button>
             <button style={buttonStyle}>MODE 3</button>
