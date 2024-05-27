69e3d45aa4e0effa4ed708c4c55809ed30557009
antihypnotic <gwakgyohap@gmail.com>
2024-05-27 10:15:26 +0900

    readme 수정

--- my-rne-app/src/js/App.js
+++ my-rne-app/src/js/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 export default function App() {
+
+    // CSS 안돼서 포기함
+    const buttonStyle = {
+        background: "#275efe",
+        color: "#fff",
+        fontSize: "15px",
+        fontFamily: "roboto",
+        display: "inline-block",
+        borderRadius: "20px",
+        border: "none"
+    }
+
     return(
-        <h1>I am App Component</h1>
+        <div>
+            <button style={buttonStyle}>MODE 1</button>
+            <button style={buttonStyle}>MODE 2</button>
+            <button style={buttonStyle}>MODE 3</button>
+            <button style={buttonStyle}>MODE 4</button>
+        </div>
     )
 }
