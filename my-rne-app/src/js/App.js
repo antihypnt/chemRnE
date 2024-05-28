28bf10842b2a357e2d4519c5a24d1579c20a96f1
antihypnotic <gwakgyohap@gmail.com>
2024-05-28 14:54:35 +0900

    userName, projectName 추가

--- my-rne-app/src/js/App.js
+++ my-rne-app/src/js/App.js
@@ -3,6 +3,24 @@ import {useState} from 'react';
 
 export default function App() {
 
+    // 변수 영역
+    const [projectName, setProjectName] = useState();
+    const [userName, setUserName] = useState();
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    // 함수 영역
+    const saveProjectName = event => {
+        setProjectName(event.target.value);
+    };
+
+    const saveUserName = event => {
+        setUserName(event.target.value);
+    }
+
+    const saveLoggedIn = () => {
+        setLoggedIn(true);
+    }
+
     // 스타일 영역 (CSS 안돼서 포기함)
     const buttonStyle = {
         width: "100px",
@@ -18,19 +36,32 @@ export default function App() {
         margin: "10px 30px 0px 30px"
     }
 
-    const divStyle = {
+    const center = {
         width: "770px",
         height: "520px",
         textAlign: "center",
         background: "#FFF"
     }
 
+
     return(
-        <div style={divStyle}>
-            <button style={buttonStyle}>MODE 1</button>
-            <button style={buttonStyle}>MODE 2</button>
-            <button style={buttonStyle}>MODE 3</button>
-            <button style={buttonStyle}>MODE 4</button>
+        <div>
+
+            {loggedIn && <div style={center}>
+                <div>{userName}의 {projectName}</div>
+                <button style={buttonStyle}>MODE 1</button>
+                <button style={buttonStyle}>MODE 2</button>
+                <button style={buttonStyle}>MODE 3</button>
+                <button style={buttonStyle}>MODE 4</button>
+            </div>}
+
+            {!loggedIn && <div style={{textAlign: "center"}}>
+                <h3>프로젝트명을 입력하세요.</h3>
+                <input onChange={saveUserName}/><br/>
+                <input onChange={saveProjectName}/><br/>
+                <button style={buttonStyle} onClick={saveLoggedIn}>다음</button>
+            </div>}
+
         </div>
     )
 }
