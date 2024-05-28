6b36a76226c204833cc4e486e13e8453934b4ccd
antihypnotic <gwakgyohap@gmail.com>
2024-05-28 16:34:20 +0900

    FastAPI 추가

--- my-rne-app/src/js/App.js
+++ my-rne-app/src/js/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useState} from 'react';
 
+
 export default function App() {
 
     // 변수 영역
---
+++ my-rne-app/src/py/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/default")
+async def default():
+    # GPIO 사용
+    return {}
+
+@app.get("/sample")
+async def sample():
+    # GPIO 사용
+    return {}
+
+@app.get("/unknown")
+async def unknown():
+    # GPIO 사용
+    return {}
